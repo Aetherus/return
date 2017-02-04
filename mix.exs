ebdefbb1f4ae334af62463fe8c0a56eaf1def754
zhoumh <zhoumh.vic@vsc.com>
2017-02-04 13:16:25 +0800

    v0.0.1

--- mix.exs
+++ mix.exs
@@ -3,11 +3,31 @@ defmodule Return.Mixfile do
 
   def project do
     [app: :return,
-     version: "0.0.1.alpha1",
+     version: "0.0.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package()
+    ]
+  end
+
+  def description do
+    """
+    Provide the return keyword and the context where return works.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Aetherus Zhou"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/aetherus/return/blob/master/lib/return.ex"
+      }
+    ]
   end
 
   # Configuration for the OTP application
@@ -28,6 +48,8 @@ defmodule Return.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
   end
 end
